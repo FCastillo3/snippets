49effe6364d9ac555d5a2fb76fb48f7a07bc9266
FCastillo3 <32094875+FCastillo3@users.noreply.github.com>
2023-10-05 07:48:18 +0700

    Delete FrostbiteImport.py
    Deleted

--- FrostbiteImport.py
+++
@@ -1,8 +0,0 @@
-#import required assemblies
-import System
-import clr
-import IronPython
-#import debugging dll to expose the m_scriptRuntime (this work-around illustrates debugging capabilities beyond the recommended workflow)
-dataDir = System.Environment.GetEnvironmentVariable("GAME_DATA_DIR")
-clr.AddReferenceToFileAndPath( dataDir + r"\Source\FBScripts\Examples\Debugging\Expose.dll", "Expose.dll")
-import Expose
